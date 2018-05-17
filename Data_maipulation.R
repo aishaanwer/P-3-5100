f48c597c4fc243db20c2dbb71ff6ebe63feda0a0
Aisha <37453150+aishaanwer@users.noreply.github.com>
2018-05-16 22:45:41 -0400

    Add files via upload

--- Data_maipulation.R
+++ Data_maipulation.R
@@ -1,13 +1,13 @@
 library(tidyverse)
 library(lubridate)
 library(dplyr)
+library(reshape2)
 
 
 
 #read the csv files
 travel_mode <- read_csv("travel.csv")   #reading the commute files
 annual_wage <- read_csv("avgwage.csv")  #reading the wage file
-
 
 
 
@@ -27,7 +27,8 @@ travel_mode$`Travel Mode` <- gsub(pattern ="Car, truck, or van:",travel_mode$`Tr
 travel_mode <- na.omit(travel_mode)
 
 #deleting the Margin of Error Column
-travel_mode <- subset(travel_mode, select = -c(`Margin of Error`))
+travel_mode <- subset(travel_mode, select = -c(`Margin of Error`, `Metropolitan Statistical Area FIPS Code`, `Metropolitan Statistical Area of County`,
+                                               `R Metropolitan Statistical Area FIPS Code`, `R  Metropolitan Statistical Area of County`))
 
 #reading in the avg annual wage column only for California and New york states
 annual_wage <- subset(annual_wage, select = c(`Annual Average Pay`, `St Name`, `Area`, `Cnty`))
@@ -49,22 +50,27 @@ travel_mode <- travel_mode%>%
          | str_detect(`County Name`, "Solana") | str_detect(`County Name`, "Marin") | str_detect(`County Name`, "Sant Mataeo") 
          | str_detect(`County Name`, "Sonoma") | str_detect(`County Name`, "Manhattan") | str_detect(`County Name`, "Bronx")
          | str_detect(`County Name`, "Brooklyn") | str_detect(`County Name`, "Queens") | str_detect(`County Name`, "Staten Island"))
+      
+
+travel_mode <- travel_mode%>%
+  filter(`R County Name`!= `County Name`)
 
 
-
+  
 #Filtering out counties outside of Bay Aread and New York City in wage dataset
 annual_wage <- annual_wage%>%
   filter(str_detect(`Area`, "Alameda") | str_detect(`Area`, "Napa") | str_detect(`Area`, "Santa Clara") 
          | str_detect(`Area`, "Contra Costa")| str_detect(`Area`, "San Francisco") 
-         | str_detect(`Area`, "Solana") | str_detect(`Area`, "Marin") | str_detect(`Area`, "Sant Mataeo") 
+         | str_detect(`Area`, "Solana") | str_detect(`Area`, "Marin") | str_detect(`Area`, "San Mataeo") 
          | str_detect(`Area`, "Sonoma") | str_detect(`Area`, "Manhattan") | str_detect(`Area`, "Bronx")
          | str_detect(`Area`, "Brooklyn") | str_detect(`Area`, "Queens") | str_detect(`Area`, "Staten Island"))
 
 #Ordering Columns in wage dataset
-annual_wage <- annual_wage[,c(4,2,3,1)][order(annual_wage$`St Name`),]
 
+travel_mode <- recast(travel_mode, id.var="ID", ID~value)
 
+#
 #travel_mode <- cbind(travel_mode, annual_wage, by.x = "County FIPS Code", by.y ="Cnty", all=FALSE)
 
-write.csv(travel_mode, file = "travelmodes.csv", row.names = F)
+write.csv(travel_mode, file = "trave.csv", row.names = F)
 write.csv(annual_wage, file = "wages.csv", row.names = F)
