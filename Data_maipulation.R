e13712583109f0cc0cf5aac56324914bd00fc52a
Aisha <37453150+aishaanwer@users.noreply.github.com>
2018-05-16 22:51:55 -0400

    updated for california only

--- Data_maipulation.R
+++ Data_maipulation.R
@@ -9,13 +9,13 @@ annual_wage <- read_csv("avgwage.csv")  #reading the wage file
 
 
 
-# filtering out residential states other than California and New York
+# filtering out residential states other than California
 travel_mode <- travel_mode %>%
-  filter(str_detect(`R State Name`, "California") | str_detect(`R State Name`, "New York"))
+  filter(str_detect(`R State Name`, "California"))
 
-# filtering out work states other than California and New York
+# filtering out work states other than California
 travel_mode <- travel_mode %>%
-  filter(str_detect(`State Name`, "California") | str_detect(`State Name`, "New York"))
+  filter(str_detect(`State Name`, "California"))
 
 #removing the Car, truck or van text from Travel Mode to simplify to only the result of the vehicle
 
@@ -32,7 +32,7 @@ travel_mode <- subset(travel_mode, select = -c(`Margin of Error`, `Metropolitan 
 annual_wage <- subset(annual_wage, select = c(`Annual Average Pay`, `St Name`, `Area`, `Cnty`))
 
 annual_wage <- annual_wage%>%
-  filter(str_detect(`St Name`, "California") | str_detect(`St Name`, "New York")) 
+  filter(str_detect(`St Name`, "California")) 
 
 
 annual_wage <- annual_wage %>%
@@ -46,8 +46,7 @@ travel_mode <- travel_mode%>%
   filter(str_detect(`County Name`, "Alameda") | str_detect(`County Name`, "Napa") | str_detect(`County Name`, "Santa Clara") 
          | str_detect(`County Name`, "Contra Costa")| str_detect(`County Name`, "San Francisco") 
          | str_detect(`County Name`, "Solana") | str_detect(`County Name`, "Marin") | str_detect(`County Name`, "Sant Mataeo") 
-         | str_detect(`County Name`, "Sonoma") | str_detect(`County Name`, "Manhattan") | str_detect(`County Name`, "Bronx")
-         | str_detect(`County Name`, "Brooklyn") | str_detect(`County Name`, "Queens") | str_detect(`County Name`, "Staten Island"))
+         | str_detect(`County Name`, "Sonoma"))
       
 
 travel_mode <- travel_mode%>%
@@ -55,13 +54,12 @@ travel_mode <- travel_mode%>%
 
 
   
-#Filtering out counties outside of Bay Aread and New York City in wage dataset
+#Filtering out counties outside of Bay Aread in wage dataset
 annual_wage <- annual_wage%>%
   filter(str_detect(`Area`, "Alameda") | str_detect(`Area`, "Napa") | str_detect(`Area`, "Santa Clara") 
          | str_detect(`Area`, "Contra Costa")| str_detect(`Area`, "San Francisco") 
          | str_detect(`Area`, "Solana") | str_detect(`Area`, "Marin") | str_detect(`Area`, "San Mataeo") 
-         | str_detect(`Area`, "Sonoma") | str_detect(`Area`, "Manhattan") | str_detect(`Area`, "Bronx")
-         | str_detect(`Area`, "Brooklyn") | str_detect(`Area`, "Queens") | str_detect(`Area`, "Staten Island"))
+         | str_detect(`Area`, "Sonoma"))
 
 #Ordering Columns in wage dataset
 
